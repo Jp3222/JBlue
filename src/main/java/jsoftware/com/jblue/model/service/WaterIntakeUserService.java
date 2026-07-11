94f310455d47693dbf418d82da0f74c034d6e4c5
Jp3222 <juanpc.559@gmail.com>
2026-07-10 19:32:25 -0600

    JPCC > 10/07/2026 MOD: SE AGREGA LA LOGICA PARA GUARDAR UN PROCESO

--- src/main/java/jsoftware/com/jblue/model/service/WaterIntakeUserService.java
+++ src/main/java/jsoftware/com/jblue/model/service/WaterIntakeUserService.java
@@ -4,8 +4,12 @@
  */
 package jsoftware.com.jblue.model.service;
 
+import java.sql.SQLException;
+import jsoftware.com.jblue.model.constants.Const;
+import jsoftware.com.jblue.model.dao.HistoryDAO;
 import jsoftware.com.jblue.model.dao.WaterIntakeUserDAO;
-import jsoftware.com.jblue.model.dto.wrp.ProcessWrapperDTO;
+import jsoftware.com.jblue.model.dto.WaterIntakeUserDTO;
+import jsoftware.com.jblue.model.exp.DataAccesObjectException;
 import jsoftware.com.jblue.model.models.AbstractService;
 import jsoftware.com.jutil.db.JDBConnection;
 
@@ -16,14 +20,33 @@ import jsoftware.com.jutil.db.JDBConnection;
 public class WaterIntakeUserService extends AbstractService {
 
     private WaterIntakeUserDAO wki_user;
+    private HistoryDAO hys;
 
     public WaterIntakeUserService(boolean dev_flag, String process_name) {
         super(dev_flag, process_name);
         wki_user = new WaterIntakeUserDAO(dev_flag, user_message);
+        hys = HistoryDAO.getInstance();
     }
 
-    public void saveProcess(JDBConnection connection, String process_type, ProcessWrapperDTO dto) {
-
+    public boolean saveProcess(JDBConnection connection, WaterIntakeUserDTO dto) {
+        boolean res = false;
+        try {
+            int insert = wki_user.insert(connection, dto);
+            res = insert > 0;
+            if (!res) {
+                returnMessageError("TOMA DE AGUA NO REGISTRADA");
+            }
+            res = hys.insert(connection, Const.INDEX_WKI_USER, "SE REGISTRO EL TITULAR: %s - %s".formatted(dto.getId(), dto.getUserName()));
+            if (!res) {
+                returnMessageError("REGISTRO EN BITACORA CORRUPTO");
+            }
+            res = true;
+        } catch (SQLException ex) {
+            returnMessageError(ex.getErrorCode(), ex.getMessage());
+        } catch (DataAccesObjectException ex) {
+            returnMessageError(ex.getErrorCode(), ex.getMessage());
+        }
+        return res;
     }
 
 }
